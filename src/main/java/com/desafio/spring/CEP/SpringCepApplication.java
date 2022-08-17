40b4a46391c2a695117debb5647680abebc2b207
Hugo Ribeiro <hugo.ribeiro@inmetrics.com.br>
2022-08-17 11:11:05 -0300

    Initial commit

---
+++ src/main/java/com/desafio/spring/CEP/SpringCepApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.spring.CEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/spring/CEP/SpringCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.spring.CEP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
