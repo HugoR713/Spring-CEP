d7073266c803df9a005612c5f7f5ab6eb6e59c7a
Hugo Ribeiro <hugo.ribeiro@inmetrics.com.br>
2022-08-17 15:53:00 -0300

    Criando as classes

--- src/main/java/com/desafio/spring/CEP/SpringCepApplication.java
+++ src/main/java/com/desafio/spring/CEP/SpringCepApplication.java
@@ -2,8 +2,10 @@ package com.desafio.spring.CEP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringCepApplication {
 
 	public static void main(String[] args) {
